e4d201833d2dc4d1eaf83fc19f63d77c62387890
Stefano Lusardi <lusardi.stefano@gmail.com>
2021-11-03 01:03:33 +0100

    Added dockerfiles for gcc and clang on ubuntu:20.04 and debian:11

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,1 +1,3 @@
-docker run --rm -it --name docker_cpp dkpp:1
+docker run --rm -it --name docker_cpp_gcc dkpp:gcc
+docker run --rm -it --name docker_cpp_clang dkpp:clang
+docker run --rm -it --name docker_cpp_debian dkpp:debian
